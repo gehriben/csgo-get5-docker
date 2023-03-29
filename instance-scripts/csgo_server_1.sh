aea4bb39a59f9861c5798f606db12b23c0520cfe
gehriben <45774453+gehriben@users.noreply.github.com>
2023-03-29 14:58:20 +0200

    Changed mappool in instance script

--- instance-scripts/csgo_server_1.sh
+++ instance-scripts/csgo_server_1.sh
@@ -5,8 +5,8 @@ docker run -it --network=host \
  -e PORT=27115 \
  -e GOTV_PORT=27120 \
  -e MATCH_CONFIG="{'matchid': '81a99ef9a2844c278c2bda2f5a77a793', \
-                   'num_maps': 3, \
-                   'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
+                   'num_maps': 1, \
+                   'maplist': ['de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_vertigo', 'de_ancient', 'de_anubis'], \
                    'skip_veto': False }" \
  csgo
 
