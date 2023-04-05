cc9c3f0034f3c363a86aa8a9c0cf683ba2d70802
gehriben <45774453+gehriben@users.noreply.github.com>
2023-04-05 18:29:18 +0200

    Updated server script

--- instance-scripts/csgo_server_1.sh
+++ instance-scripts/csgo_server_1.sh
@@ -2,25 +2,11 @@ docker run -it --network=host \
  -e PASSWORD=password \
  -e RCON_PASSWORD=rconpassword \
  -e GOTV_PASSWORD=gtovpassword \
- -e PORT=27116 \
- -e GOTV_PORT=27121 \
+ -e PORT=27115 \
+ -e GOTV_PORT=27120 \
  -e MATCH_CONFIG="{'matchid': '81a99ef9a2844c278c2bda2f5a77a793', \
                    'num_maps': 3, \
                    'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
-                   'skip_veto': False, \
-                   'team1': {'name': 'Astralis', \
-                             'tag': 'AST', \
-                             'players': {698652696634933762: 'gla1ve', \
-                                         234783204182937471: 'Magisk', \
-                                         389371614622221912: 'dev1ce', \
-                                         951311418417028314: 'dupreeh', \
-                                         369417162788295143: 'Xyp9x'}}, \
-                   'team2': {'name': 'Natus Vincere', \
-                             'tag': 'NAVI', \
-                             'players': {875407653610178066: 's1mple', \
-                                         979550479724346962: 'Boombl4', \
-                                         186841562108230104: 'electronic', \
-                                         726408891643982724: 'Perfecto', \
-                                         512316566954794515: 'flamie'}}}" \
+                   'skip_veto': False } \
  csgo
 
