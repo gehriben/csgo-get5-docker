9d0ef65e64544c9f811674c2e07aca0c2e36ca93
gehriben <45774453+gehriben@users.noreply.github.com>
2023-04-06 08:14:28 +0200

    Update csgo_server_2.sh

--- instance-scripts/csgo_server_2.sh
+++ instance-scripts/csgo_server_2.sh
@@ -4,6 +4,7 @@ docker run -it --network=host \
  -e GOTV_PASSWORD=gtovpassword \
  -e PORT=27115 \
  -e GOTV_PORT=27120 \
+ -e CUSTOM_ARGS='+sv_lan 0'
  -e MATCH_CONFIG="{'num_maps': 3, \
                    'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
                    'skip_veto': False }" \
