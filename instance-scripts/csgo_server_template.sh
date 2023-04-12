e38e4bb17e2f215bfa7f3eb15e30539dd7afdf25
gehn <gehn@zhaw.ch>
2023-04-12 14:54:41 +0200

    Create server start creation script

--- instance-scripts/csgo_server_template.sh
+++ instance-scripts/csgo_server_template.sh
@@ -2,9 +2,9 @@ docker run -dt --network=host \
  -e PASSWORD=password \
  -e RCON_PASSWORD=rconpassword \
  -e GOTV_PASSWORD=gtovpassword \
- -e PORT=27118 \
- -e GOTV_PORT=27123 \
- -e MATCH_CONFIG="{'num_maps': 3, \
+ -e PORT=27001 \
+ -e GOTV_PORT=27101 \
+ -e MATCH_CONFIG="{'num_maps': 1, \
                    'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
                    'skip_veto': 0, \
                    'veto_first': 'team1', \
@@ -15,4 +15,3 @@ docker run -dt --network=host \
                              'flag': 'DE'}, \
                    'cvars': {'hostname': 'CSGO Server'}}" \
  gehriben/csgo
-
