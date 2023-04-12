38e2f8fd667755e529d2500df24abc3a13c73f93
gehn <gehn@zhaw.ch>
2023-04-12 14:59:31 +0200

    Fixed creation script

--- instance-scripts/create_scripts.py
+++ instance-scripts/create_scripts.py
@@ -6,22 +6,22 @@ for i in range(2):
     gotv_port = START_GOTV_PORT+i
 
     f = open("bo1/csgo_server_"+str(i+1)+".sh", "w")
-    f.write("docker run -dt --network=host \\ \n \
--e PASSWORD=password \\ \n \
--e RCON_PASSWORD=rconpassword \\ \n \
--e GOTV_PASSWORD=gtovpassword \\ \n \
--e PORT="+str(port)+" \\ \n \
--e GOTV_PORT="+str(gotv_port)+" \\ \n \
--e MATCH_CONFIG=\"{\'num_maps\': 1, \\ \n\
-                   \'maplist\': [\'de_dust2\', \'de_inferno\', \'de_mirage\', \'de_nuke\', \'de_overpass\', \'de_train\', \'de_vertigo\'], \\ \n \
-                  \'skip_veto\': 0, \\ \n \
-                  \'veto_first\': \'team1\', \\ \n \
-                  \'side_type\': \'standard\', \\ \n \
-                  \'team1\': {\'name\': \'Team 1\', \\ \n \
-                            \'flag\': \'DE\'}, \\ \n \
-                  \'team2\': {\'name\': \'Team 2\', \\ \n \
-                            \'flag\': \'DE\'}, \\ \n \
-                  \'cvars\': {\'hostname\': \'CSGO Server\'}}\" \\ \n \
+    f.write("docker run -dt --network=host \\\n \
+-e PASSWORD=password \\\n \
+-e RCON_PASSWORD=rconpassword \\\n \
+-e GOTV_PASSWORD=gtovpassword \\\n \
+-e PORT="+str(port)+" \\\n \
+-e GOTV_PORT="+str(gotv_port)+" \\\n \
+-e MATCH_CONFIG=\"{\'num_maps\': 1, \\\n\
+                   \'maplist\': [\'de_dust2\', \'de_inferno\', \'de_mirage\', \'de_nuke\', \'de_overpass\', \'de_train\', \'de_vertigo\'], \\\n \
+                  \'skip_veto\': 0, \\\n \
+                  \'veto_first\': \'team1\', \\\n \
+                  \'side_type\': \'standard\', \\\n \
+                  \'team1\': {\'name\': \'Team 1\', \\\n \
+                            \'flag\': \'DE\'}, \\\n \
+                  \'team2\': {\'name\': \'Team 2\', \\\n \
+                            \'flag\': \'DE\'}, \\\n \
+                  \'cvars\': {\'hostname\': \'CSGO Server\'}}\" \\\n \
 gehriben/csgo \
 ")
     f.close() 
