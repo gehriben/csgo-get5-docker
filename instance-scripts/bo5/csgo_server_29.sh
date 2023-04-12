7949c1ff041b22c0cb18889f0810f225232b779d
gehn <gehn@zhaw.ch>
2023-04-12 15:04:32 +0200

    Added all instance scripts

---
+++ instance-scripts/bo5/csgo_server_29.sh
@@ -0,0 +1,17 @@
+docker run -dt --network=host \
+ -e PASSWORD=password \
+ -e RCON_PASSWORD=rconpassword \
+ -e GOTV_PASSWORD=gtovpassword \
+ -e PORT=27029 \
+ -e GOTV_PORT=27129 \
+ -e MATCH_CONFIG="{'num_maps': 5, \
+                   'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
+                   'skip_veto': 0, \
+                   'veto_first': 'team1', \
+                   'side_type': 'standard', \
+                   'team1': {'name': 'Team 1', \
+                             'flag': 'DE'}, \
+                   'team2': {'name': 'Team 2', \
+                             'flag': 'DE'}, \
+                   'cvars': {'hostname': 'CSGO Server'}}" \
+ gehriben/csgo 
--- instance-scripts/create_scripts.py
+++ instance-scripts/create_scripts.py
@@ -1,18 +1,19 @@
 START_PORT = 27001
 START_GOTV_PORT = 27101
+SCRIPT_COUNT = 32
 
-for i in range(2):
+for i in range(SCRIPT_COUNT):
     port = START_PORT+i
     gotv_port = START_GOTV_PORT+i
 
-    f = open("bo1/csgo_server_"+str(i+1)+".sh", "w")
+    f = open("bo5/csgo_server_"+str(i+1)+".sh", "w")
     f.write("docker run -dt --network=host \\\n \
 -e PASSWORD=password \\\n \
 -e RCON_PASSWORD=rconpassword \\\n \
 -e GOTV_PASSWORD=gtovpassword \\\n \
 -e PORT="+str(port)+" \\\n \
 -e GOTV_PORT="+str(gotv_port)+" \\\n \
--e MATCH_CONFIG=\"{\'num_maps\': 1, \\\n\
+-e MATCH_CONFIG=\"{\'num_maps\': 5, \\\n\
                    \'maplist\': [\'de_dust2\', \'de_inferno\', \'de_mirage\', \'de_nuke\', \'de_overpass\', \'de_train\', \'de_vertigo\'], \\\n \
                   \'skip_veto\': 0, \\\n \
                   \'veto_first\': \'team1\', \\\n \
