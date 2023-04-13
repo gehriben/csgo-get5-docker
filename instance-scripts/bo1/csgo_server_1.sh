c0178db373f7fe626b3ac39808e221649f458883
gehriben <45774453+gehriben@users.noreply.github.com>
2023-04-13 09:39:11 +0200

    Update csgo_server_1.sh

--- instance-scripts/bo1/csgo_server_1.sh
+++ instance-scripts/bo1/csgo_server_1.sh
@@ -1,4 +1,4 @@
-docker run -dt --network=host \
+docker run -it --network=host \
  -e SERVER_TOKEN=12C4627A69E1B096045E335622BF9FDF \
  -e PASSWORD=password \
  -e RCON_PASSWORD=rconpassword \
@@ -6,14 +6,7 @@ docker run -dt --network=host \
  -e PORT=27001 \
  -e GOTV_PORT=27101 \
  -e CUSTOM_ARGS="+sv_lan 0" \
- -e MATCH_CONFIG="{'num_maps': 1, \
-                   'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
-                   'skip_veto': 0, \
-                   'veto_first': 'team1', \
-                   'side_type': 'standard', \
-                   'team1': {'name': 'Team 1', \
-                             'flag': 'DE'}, \
-                   'team2': {'name': 'Team 2', \
-                             'flag': 'DE'}, \
-                   'cvars': {'hostname': 'CSGO Server'}}" \
+ -e MATCH_CONFIG="{\"num_maps\": 1, \
+                   \"maplist\": [\"de_dust2\", \"de_inferno\", \"de_mirage\", \"de_nuke\", \"de_overpass\", \"de_train\", \"de_vertigo\"], \
+                   \"skip_veto\": false}" \
  gehriben/csgo 
