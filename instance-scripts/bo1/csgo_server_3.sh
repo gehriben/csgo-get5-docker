9acb91d6295dbe3827f5fb6f386455f5dd2b5c05
gehriben <45774453+gehriben@users.noreply.github.com>
2023-04-13 08:57:46 +0200

    Update csgo_server_3.sh

--- instance-scripts/bo1/csgo_server_3.sh
+++ instance-scripts/bo1/csgo_server_3.sh
@@ -1,9 +1,11 @@
 docker run -dt --network=host \
+ -e SERVER_TOKEN=12C4627A69E1B096045E335622BF9FDF \
  -e PASSWORD=password \
  -e RCON_PASSWORD=rconpassword \
  -e GOTV_PASSWORD=gtovpassword \
  -e PORT=27003 \
  -e GOTV_PORT=27103 \
+ -e CUSTOM_ARGS="+sv_lan 0" \
  -e MATCH_CONFIG="{'num_maps': 1, \
                    'maplist': ['de_dust2', 'de_inferno', 'de_mirage', 'de_nuke', 'de_overpass', 'de_train', 'de_vertigo'], \
                    'skip_veto': 0, \
