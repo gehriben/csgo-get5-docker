6ec6f48613f37f63ed8d5e158895124870a8e456
gehriben <45774453+gehriben@users.noreply.github.com>
2023-04-13 09:45:58 +0200

    Update csgo_server_1.sh

--- instance-scripts/bo1/csgo_server_1.sh
+++ instance-scripts/bo1/csgo_server_1.sh
@@ -7,5 +7,12 @@ docker run -it --network=host \
  -e CUSTOM_ARGS="+sv_lan 0" \
  -e MATCH_CONFIG="{\"num_maps\": 1, \
                    \"maplist\": [\"de_dust2\", \"de_inferno\", \"de_mirage\", \"de_nuke\", \"de_overpass\", \"de_train\", \"de_vertigo\"], \
-                   \"skip_veto\": false}" \
+                   \"skip_veto\": false, 
+                   \"veto_first\": \"team1\", \
+                   \"side_type\": \"standard\", \
+                   \"team1\": {\"name\": \"Team 1\", \
+                             \"flag\": \"DE\"}, \
+                   \"team2\": {\"name': \"Team 2\", \
+                             \"flag\": \"DE\"}, \
+                   \"cvars\": {\"hostname\": \"CSGO Server\"}}" \}" \
  gehriben/csgo 
